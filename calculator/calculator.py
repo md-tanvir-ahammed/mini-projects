47330872b58e229d806a7aeae4c0b1bac15b98ee
Md. Tanvir Ahammed <mdtanvirahammed10@gmail.com>
2026-03-20 16:24:59 +0600

    added calculator project in Python

---
+++ calculator/calculator.py
@@ -0,0 +1,26 @@
+def calculator():
+    print("===== Simple Calculator =====")
+    
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+    
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 == 0:
+            print("Error! Division by zero.")
+            return
+        else:
+            result = num1 / num2
+    else:
+        print("Invalid operator!")
+        return
+    
+    print(f"Result: {result}")
+
+calculator()
